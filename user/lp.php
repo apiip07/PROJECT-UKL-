daac6b18fd9a9869b37d2ee18e4783ec640fcfba
apiip07 <arafif305@gmail.com>
2024-02-25 23:34:39 +0700

    First Commit

---
+++ user/lp.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing page</title>
+    <link rel="stylesheet" href="landing1.css">
+
+    <!-- parallax js -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="parallax.js/path/to/parallax.js"></script>
+</head>
+
+<body>
+
+    <!-- header -->
+    <header class="parallax-window" data-parallax="scroll" data-image-src="image/header-bg.jpg">
+        <nav>
+            <h1 class="logo">
+                <a href="/">Indonesia</a>
+            </h1>
+            <a href="../index.php" class="btn-sign-up">Log Out</a>
+        </nav>
+        <div class="header-title">Sumber Daya Alam</div>
+        <div class="header-bottom">
+            <p class="today-date">22 <span>/02</span></p>
+            <ul class="nav-bottom">
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">More</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </div>
+    </header>
+    <!-- about -->
+    <section id="about">
+        <div class="about-container">
+            <div class="image-gallery">
+                <div class="image-box">
+                    <img src="image/img-1.jpg" alt="image">
+                    <div class="txt-img">Sumber</div>
+                </div>
+                <div class="image-box">
+                    <img src="image/img-2.jpg" alt="image">
+                    <div class="txt-img">Daya</div>
+                </div>
+                <div class="image-box">
+                    <img src="image/img-3.jpg" alt="image">
+                    <div class="txt-img">Alam</div>
+                </div>
+                <div class="image-box">
+                    <img src="image/img-4.jpg" alt="image">
+                    <div class="txt-img">Lestari</div>
+                </div>
+            </div>
+            <div class="about-info">
+               Website ini dibuat untuk mengajak kepada semua orang untuk menjaga dan melestarikan sumber daya alam yang ada di indonesia. Anda juga bisa menngeksplorasi apa saja sumber daya alam yang kita miliki di negara kita ini dari segala kategori.
+            </div>
+        </div>
+    </section>
+
+    <!-- footer -->
+    <footer>Created by Rafif Arsalan</footer>
+</body>
+
+</html>
